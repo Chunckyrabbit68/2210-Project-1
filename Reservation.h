7159bbb6af29b43df4e243b63de9ddd7a51f89b1
Chunckyrabbit68 <gwlmc@outlook.com>
2026-09-17 16:31:37 -0500

    Add Reservation class definition

--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,29 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
 
+#include <string>
+using namespace std;
+
+class Reservation {
+private:
+    int reservationID;
+    int studentID;
+    string studentName;
+    string resourceID;
+    string date;
+
+public:
+    Reservation();
+
+    Reservation(int reservationID, int studentID, string studentName, string resourceID, string date);
+
+    int getReservationID() const;
+    int getStudentID() const;
+    string getStudentName() const;
+    string getResourceID() const;
+    string getDate() const;
+
+    void display() const;
+};
+
+#endif
