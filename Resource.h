f07d18fc2e4cedd00399e0aae1ddd911d7f310b1
Chunckyrabbit68 <gwlmc@outlook.com>
2026-09-20 14:31:15 -0500

    Add Resource class definition with methods

--- Resource.h
+++ Resource.h
@@ -1,1 +1,34 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
 
+#include <string>
+using namespace std;
+
+class Resource {
+private:
+    string resourceID;
+    string resourceName;
+    string resourceType;
+    string status;
+
+public:
+    Resource();
+
+    Resource(string id,
+             string name,
+             string type,
+             string status);
+
+    string getResourceID() const;
+    string getResourceName() const;
+    string getResourceType() const;
+    string getStatus() const;
+
+    bool isAvailable() const;
+
+    void setStatus(string newStatus);
+
+    void display() const;
+};
+
+#endif
