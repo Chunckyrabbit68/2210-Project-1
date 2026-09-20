43dca2d0cd0d7360b5e81efc0599eaa9d433de8f
Chunckyrabbit68 <gwlmc@outlook.com>
2026-09-20 16:04:24 -0500

    Refactor Reservation class to include time attribute

--- Reservation.cpp
+++ Reservation.cpp
@@ -3,46 +3,33 @@
 using namespace std;
 
 Reservation::Reservation() {
-  reservationID = 0;
-  studentID = 0;
-  studentName = "";
-  resourceID = "";
-  date = "";
-  
+    reservationID = 0;
+    studentID = 0;
+    studentName = "";
+    resource = "";
+    date = "";
+    time = "";
 }
 
-Reservation::Reservation(int reservationID, int studentID, string studentName, string resourceID, string date) {
+Reservation::Reservation(int reservationID, int studentID, string studentName, string resource, string date, string time) {
     this->reservationID = reservationID;
     this->studentID = studentID;
     this->studentName = studentName;
-    this->resourceID = resourceID;
+    this->resource = resource;
     this->date = date;
-}
-
-int Reservation::getReservationID() const {
-    return reservationID;
-}
-
-int Reservation::getStudentID() const {
-    return studentID;
-}
-
-string Reservation::getStudentName() const {
-    return studentName;
-}
-
-string Reservation::getResourceID() const {
-    return resourceID;
-}
-
-string Reservation::getDate() const {
-    return date;
+    this->time = time;
 }
 
 void Reservation::display() const {
     cout << reservationID << " | "
          << studentID << " | "
          << studentName << " | "
-         << resourceID << " | "
-         << date << endl;
+         << resource << " | "
+         << date;
+
+    if (!time.empty()) {
+        cout << " | " << time;
+    }
+
+    cout << endl;
 }
